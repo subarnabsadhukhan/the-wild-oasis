62c76007cd611991adfc8c4544fbe0607c86c73c
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-02-28 21:52:54 +0530

    Introduction to Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  color: red;
+  font-size: 4rem;
+  background-color: blue;
+`;
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <H1>Hello</H1>
+    </div>
+  );
 }
 
 export default App;
