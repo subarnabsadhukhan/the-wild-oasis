006160e2be5dd4d350651102d7d91b958c27b16e
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-03-06 09:20:29 +0530

    implement React Portal using createPortal()

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { HiXMark } from "react-icons/hi2";
+import { createPortal } from "react-dom";
 const StyledModal = styled.div`
   position: fixed;
   top: 50%;
@@ -50,7 +51,7 @@ const Button = styled.button`
 `;
 
 function Modal({ children, onClose }) {
-  return (
+  return createPortal(
     <Overlay>
       <StyledModal>
         <Button onClick={onClose}>
@@ -58,7 +59,8 @@ function Modal({ children, onClose }) {
         </Button>
         <div>{children}</div>
       </StyledModal>
-    </Overlay>
+    </Overlay>,
+    document.body
   );
 }
 
