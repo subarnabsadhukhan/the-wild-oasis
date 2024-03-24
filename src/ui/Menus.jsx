db5e707b7649744294d4e086f57bb9b7c4409856
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-03-24 11:03:43 +0530

    Bug fix: concept- event propagation and capture, fix select box not close on click issue

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -87,6 +87,7 @@ function Toggle({ id, disabled = false }) {
   const { open, close, openId, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -103,7 +104,7 @@ function Toggle({ id, disabled = false }) {
 }
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const clickRef = useOutsideClick(close);
+  const clickRef = useOutsideClick(close, false);
   if (openId !== id) return null;
   return createPortal(
     <StyledList ref={clickRef} $position={position}>
